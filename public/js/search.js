61fddd41edc07ab28ee8a0756b48250324c8a30d
Nicolas Fontan <nico.fontan1@gmail.com>
2020-10-25 13:09:25 -0400

    Made some overarching changes: songsearch and search now match eachother exactly thematically and both display a loading message until the axios call is finished and appends things to the page, index also has a list of top 20 songs now that will display loading until it's appended, made an API route for getting the top 20 songs, made some CSS changes, added more to the search js so that when you're on the index page it will create the top20 songs list and make it so you can click on an item in the list and it will take you to the songsearch page for that song.

--- public/js/search.js
+++ public/js/search.js
@@ -9,7 +9,6 @@ $(document).ready(() => {
   });
 
   function search(searchTerm) {
-    console.log(searchTerm);
     storedSearch = searchTerm;
     localStorage.setItem("storedSearch", JSON.stringify(storedSearch));
     window.location.replace("/search");
@@ -31,9 +30,10 @@ $(document).ready(() => {
   if (window.location.pathname === "/search") {
     init();
     $.get("/api/searchArtist/" + storedSearch).then(response => {
-      $("#strArtist").text(response.strArtist);
-      $("#strGenre").text(response.strGenre);
-      $("#strMood").text(response.strMood);
+      $("#strArtistThumb").attr("src", response.strArtistThumb);
+      $("#strArtist").text("Artist: " + response.strArtist);
+      $("#strGenre").text("Genre: " + response.strGenre);
+      $("#strMood").text("Mood: " + response.strMood);
       $("#strFacebook").text(response.strFacebook);
       $("#strFacebook").attr("href", "https://" + response.strFacebook);
       $("#strLastFMChart").text(response.strLastFMChart);
@@ -44,6 +44,8 @@ $(document).ready(() => {
 
       $.get("/api/artist/" + storedSearch).then(response => {
         const topSongsEl = $("#topSongs");
+        const topTextEl = $("#topText");
+        topTextEl.text("Top " + response.length);
         for (let i = 0; i < response.length; i++) {
           const liEl = $("<li>");
           liEl.attr("song-id", response[i].idTrack);
@@ -62,17 +64,38 @@ $(document).ready(() => {
   } else if (window.location.pathname === "/songsearch") {
     init();
     $.get("/api/searchSong/" + storedSearch).then(response => {
-      console.log(response);
       $("#strTrackThumb").attr("src", response.strTrackThumb);
-      $("#strTrack").text(response.strTrack);
-      $("#strArtist").text(response.strArtist);
-      $("#strAlbum").text(response.strAlbum);
-      $("#strGenre").text(response.strGenre);
+      $("#strTrack").text("Song: " + response.strTrack);
+      $("#strArtist").text("Artist: " + response.strArtist);
+      $("#strAlbum").text("Album: " + response.strAlbum);
+      $("#strGenre").text("Genre: " + response.strGenre);
       const strMusicVid =
         "https://www.youtube.com/embed/" +
-        response.strMusicVid.substring(32, response.strMusicVid.length);
+        response.strMusicVid.substring(
+          response.strMusicVid.indexOf("=") + 1,
+          response.strMusicVid.length
+        );
       $("#strMusicVid").attr("src", strMusicVid);
       $("#strDescriptionEN").text(response.strDescriptionEN);
+    });
+  } else if (window.location.pathname === "/") {
+    $.get("/api/top20/" + storedSearch).then(response => {
+      const topSongsEl = $("#top20");
+      const loadingEl = $("#loading");
+      for (let i = 0; i < 20; i++) {
+        const liEl = $("<li>");
+        liEl.attr("song-id", response[i].idTrack);
+        liEl.addClass("topSongItem");
+        liEl.text(response[i].strTrack + " - " + response[i].strArtist);
+
+        topSongsEl.append(liEl);
+      }
+      loadingEl.remove();
+    });
+    $("#top20").on("click", event => {
+      const element = event.target;
+      const id = element.getAttribute("song-id");
+      songSearch(id);
     });
   }
 });
